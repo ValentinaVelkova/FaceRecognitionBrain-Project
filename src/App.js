0861db64a8a4ea1cb8128e87bc3be5a0dd40fa96
ValentinaVelkova <166612677+ValentinaVelkova@users.noreply.github.com>
2024-07-04 18:18:32 +0300

    Add Clarifai API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,112 @@
-import './App.css';
+/* eslint-disable no-useless-concat */
+import React, { Component } from 'react';
+import ParticlesBg from 'particles-bg'
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
-import ParticlesBg from 'particles-bg'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import './App.css';
 
 
+const app = new Clarifai.App({
+  apiKey: 'a6d8782a88d84ce9a536d0df89696caf'
+});
+
+const returnClarifaiRequestOprions =(imageUrl) => {
+  const PAT = 'c74aec1c0b7b497d8a2ecfbdd4885af8';
+  const USER_ID = 'fusnk12vfifx';       
+  const APP_ID = 'facerecognitionbrain';
+  const MODEL_ID = 'face-detection';
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                }
+            }
+        }
+    ]
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+    },
+    body: raw
+  };
+
+  return requestOptions;
+ }
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
 
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg type="cobweb" bg={true}  num={80} color='#F5F5F5'/>
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box})
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input})
+
+    fetch("https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs", returnClarifaiRequestOprions(this.state.input))
+    .then(response => response.json())
+    .then((response) => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ParticlesBg type="cobweb" bg={true}  num={80} color='#F5F5F5'/>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit = {this.onButtonSubmit} 
+        />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  }
 }
 
 export default App;
